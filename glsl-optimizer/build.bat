4c5ba1c7601b58248dec22786cd027536b4b795f
Virgile Bello <virgile@siliconstudio.co.jp>
2014-10-22 17:02:06 +0900

    Release 1.0.0-beta02

--- glsl-optimizer/build.bat
+++ glsl-optimizer/build.bat
@@ -1,12 +1,5 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
-msbuild ..\..\externals\glsl-optimizer\projects\vs2013\glsl_optimizer.sln /Property:Configuration=Release;Platform="Win32"
-msbuild ..\..\externals\glsl-optimizer\projects\vs2013\glsl_optimizer.sln /Property:Configuration=Debug;Platform="Win32"
-
-mkdir include
-copy ..\..\externals\glsl-optimizer\src\glsl\glsl_optimizer.h include\
-
-mkdir lib\release
-copy ..\..\externals\glsl-optimizer\projects\vs2013\build\Win32\Release\glsl_optimizer_lib-win32.lib lib\release\
-
-mkdir lib\debug
-copy ..\..\externals\glsl-optimizer\projects\vs2013\build\glsl_optimizer_lib\Debug\glsl_optimizer_lib-win32.lib lib\debug\
+msbuild ..\..\externals\glsl-optimizer\projects\vs2013\glsl_optimizer_lib.vcxproj /Property:Configuration=Release_Dll;Platform="Win32"
+msbuild ..\..\externals\glsl-optimizer\projects\vs2013\glsl_optimizer_lib.vcxproj /Property:Configuration=Release_Dll;Platform="x64"
+xcopy /Y ..\..\externals\glsl-optimizer\projects\vs2013\build\glsl_optimizer_lib\Win32\Release_Dll\*.dll Windows\x86\glsl_optimizer.dll
+xcopy /Y ..\..\externals\glsl-optimizer\projects\vs2013\build\glsl_optimizer_lib\x64\Release_Dll\*.dll Windows\x64\glsl_optimizer.dll
