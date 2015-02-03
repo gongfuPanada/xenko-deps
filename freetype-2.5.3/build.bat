52f1aa69e234bbbef44110fc9e63b5834c67d63b
Virgile Bello <virgile@siliconstudio.co.jp>
2015-02-03 16:42:21 +0900

    Release 1.0.0-beta07

--- freetype-2.5.3/build.bat
+++ freetype-2.5.3/build.bat
@@ -1,9 +1,9 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=Win32
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=x64
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=x64
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=Win32
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=ARM
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=Win32
-msbuild ..\..\externals\freetype\builds\windows\vc2012\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=ARM
-xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2012\bin\*.dll .
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=Win32
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=x64
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=x64
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=Win32
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=ARM
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=Win32
+msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=ARM
+xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\*.dll .
