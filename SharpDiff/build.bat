7c04a54b3bd862e9f2d3b9e6e29e0eb45b96945b
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-09-30 17:49:03 +0900

    [SharpDiff] Update to latest SharpDiff to fix issue with merge

--- SharpDiff/build.bat
+++ SharpDiff/build.bat
@@ -1,8 +1,8 @@
 REM @echo off
 setlocal
 set SHARPDIFF=%~dp0..\..\externals\SharpDiff
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
-msbuild /tv:4.0 /t:Build /verbosity:quiet /clp:ErrorsOnly /fl /flp:logfile=BuildErrors.log;ErrorsOnly "/p:Configuration=Release;Platform=Any CPU" %SHARPDIFF%\SharpDiff.sln
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
+msbuild /tv:14.0 /t:Build /verbosity:quiet /clp:ErrorsOnly /fl /flp:logfile=BuildErrors.log;ErrorsOnly "/p:Configuration=Release;Platform=Any CPU" %SHARPDIFF%\SharpDiff.sln
 if NOT ERRORLEVEL 0 pause
 
 xcopy /Y %SHARPDIFF%\SharpDiff\Bin\Release\SharpDiff.dll .
