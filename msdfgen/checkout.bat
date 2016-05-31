6a2992c7c84e8622cb7f49144710e8c978230197
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-05-31 11:11:34 +0900

    [UI] msdfgen.exe copy step updated.

--- msdfgen/checkout.bat
+++ msdfgen/checkout.bat
@@ -4,9 +4,9 @@ set HOME=%USERPROFILE%
 "%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:SiliconStudio/msdfgen.git ../../externals/msdfgen
 if NOT ERRORLEVEL 0 pause
 
-"%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:leethomason/tinyxml2.git ../../externals/tinyxml2
-if NOT ERRORLEVEL 0 pause
+REM "%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:leethomason/tinyxml2.git ../../externals/tinyxml2
+REM if NOT ERRORLEVEL 0 pause
 
-"%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:lvandeve/lodepng.git ../../externals/lodepng
-if NOT ERRORLEVEL 0 pause
+REM "%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:lvandeve/lodepng.git ../../externals/lodepng
+REM if NOT ERRORLEVEL 0 pause
 
